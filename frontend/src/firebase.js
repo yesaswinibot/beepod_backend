c326cc61bae759eb33483be0e367a9d2fc8fce51
KUDIRELLI KRISHNA YESASWINI <kudirelliyesaswini@gmail.com>
2026-06-16 17:03:18 +0530

    Fix formatting and update Firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,12 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCm2Sqf2p6jHrm0kdlGA9jgxw1C3o1VFZs",
-  authDomain: "beepod.in"
+  apiKey: "AIzaSyCm2Sqf2p6jHrm0kdlGA9jgxw1C3a1VFZs",
+  authDomain: "beepod.in",
   projectId: "beepod-9d22f",
   storageBucket: "beepod-9d22f.firebasestorage.app",
   messagingSenderId: "337373620354",
   appId: "1:337373620354:web:633619d7b6e10ed0f8ac9b"
 }
-
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
